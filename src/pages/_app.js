ed20cd92bb0d5b5696a5e8bdee106acff23dfb57
Mr.Q <linxiaohua@JohnnyChens-MacBook-Pro.local>
2026-05-13 13:58:42 +0800

    Add Vercel Analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import '../../styles/globals.css';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import Head from 'next/head';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   return (
@@ -15,6 +16,7 @@ export default function App({ Component, pageProps }) {
         <meta name="apple-mobile-web-app-title" content="Kiddo Map" />
       </Head>
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
